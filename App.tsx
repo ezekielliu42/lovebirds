a1fe7b76206565e4d48a1952296194eb1b9400a2
Ezekiel Liu <liudezekiel@gmail.com>
2024-02-02 14:56:34 -0800

    reusable component TinderCard

--- App.tsx
+++ App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Text, Image, ImageBackground, View, StyleSheet } from 'react-native';
+import Card from './src/components/TinderCard';
+import users from './assets/data/users';
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Elon Musk</Text>
-            <Text style={styles.bio}>
-              A dude with a rocket is looking for a gal with fuel
-            </Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <Card user={users[0]}/>
     </View>
   );
 };
@@ -28,39 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1
   },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-    elevation: 11
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-    justifyContent: 'flex-end',
-  },
-  cardInner: {
-    padding: 10,
-  },
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
-  }
+  
 });
 
 export default App;
